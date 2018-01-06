cfdf43f72cea4e2df17706a9860fb3103e4af414
Abigail Watson <awatson1978@gmail.com>
2018-01-06 14:47:52 -0600

    http://hl7.org/fhir/json-schema/Narrative

---
+++ index.es.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+
+// exports.SimpleSchemas = _SimpleSchemas;
+exports.AuditEventSchema = require('./fhir.schema.json/AuditEvent.schema.json').AuditEventSchema;
+exports.CarePlanSchema = require('./fhir.schema.json/CarePlan.schema.json').CarePlanSchema;
+exports.DeviceSchema = require('./fhir.schema.json/Device.schema.json').DeviceSchema;
+exports.EndpointSchema = require('./fhir.schema.json/Endpoint.schema.json').EndpointSchema;
+exports.LocationSchema = require('./fhir.schema.json/Location.schema.json').LocationSchema;
+exports.MedicationSchema = require('./fhir.schema.json/Medication.schema.json').MedicationSchema;
+exports.ObservationSchema = require('./fhir.schema.json/Observation.schema.json').ObservationSchema;
+exports.OrganizationSchema = require('./fhir.schema.json/Organization.schema.json').OrganizationSchema;
+exports.PatientSchema = require('./fhir.schema.json/Patient.schema.json').PatientSchema;
+exports.PractitionerSchema = require('./fhir.schema.json/Practitioner.schema.json').PractitionerSchema;
+exports.QuestionnaireSchema = require('./fhir.schema.json/Questionnaire.schema.json').QuestionnaireSchema;
+exports.QuestionnaireResponseSchema = require('./fhir.schema.json/QuestionnaireResponse.schema.json').QuestionnaireResponseSchema;
+
