55273fbb11dec4207c3c7c3b185f5fd2d53bdcfc
Abigail Watson <awatson1978@gmail.com>
2018-01-06 02:36:44 -0600

    deprecating fhir-simple-schemas

---
+++ datatypes/CodeableConcept.js
@@ -0,0 +1,32 @@
+
+var Code = require('../datatypes/Code');
+var CodingSchema = require('../datatypes/Coding');
+var PeriodSchema = require('../datatypes/Period');
+var ReferenceSchema = require('../datatypes/Reference');
+
+
+exports.CodeableConceptSchema = new SimpleSchema({
+  "coding" : {
+    optional: true,
+    type: [ CodingSchema ]
+  },
+  "text" : {
+    optional: true,
+    type: String
+  }
+});
+
+
+exports.DefaultCodeableConcept = {
+  create: function(text){
+    var newCodeableConcept = {
+      text: ''
+    };
+
+    if (text) {
+      newCodeableConcept.text = text;
+    }
+
+    return newCodeableConcept;
+  }
+};
--- index.js
+++ index.js
@@ -5,33 +5,33 @@ Object.defineProperty(exports, '__esModule', {
 });
 
 
-const AuditEventSchema = require('./simple.schemas/AuditEvent');
-const CarePlanSchema = require('./simple.schemas/CarePlan');
-const DeviceSchema = require('./simple.schemas/Device');
-const EndpointSchema = require('./simple.schemas/Endpoint');
-const LocationSchema = require('./simple.schemas/Location');
-const MedicationSchema = require('./simple.schemas/Medication');
-const ObservationSchema = require('./simple.schemas/Observation');
-const OrganizationSchema = require('./simple.schemas/Organization');
-const PatientSchema = require('./simple.schemas/Patient');
-const PractitionerSchema = require('./simple.schemas/Practitioner');
-const QuestionnaireSchema = require('./simple.schemas/Questionnaire');
-const QuestionnaireResponseSchema = require('./simple.schemas/QuestionnaireResponse');
+// const AuditEventSchema = require('./simple.schemas/AuditEvent');
+// const CarePlanSchema = require('./simple.schemas/CarePlan');
+// const DeviceSchema = require('./simple.schemas/Device');
+// const EndpointSchema = require('./simple.schemas/Endpoint');
+// const LocationSchema = require('./simple.schemas/Location');
+// const MedicationSchema = require('./simple.schemas/Medication');
+// const ObservationSchema = require('./simple.schemas/Observation');
+// const OrganizationSchema = require('./simple.schemas/Organization');
+// const PatientSchema = require('./simple.schemas/Patient');
+// const PractitionerSchema = require('./simple.schemas/Practitioner');
+// const QuestionnaireSchema = require('./simple.schemas/Questionnaire');
+// const QuestionnaireResponseSchema = require('./simple.schemas/QuestionnaireResponse');
 
-var _SimpleSchemas = {
-  AuditEventSchema, 
-  CarePlanSchema, 
-  DeviceSchema, 
-  EndpointSchema, 
-  LocationSchema, 
-  MedicationSchema, 
-  ObservationSchema, 
-  OrganizationSchema, 
-  PatientSchema, 
-  PractitionerSchema, 
-  QuestionnaireSchema, 
-  QuestionnaireResponseSchema
-}
+// var _SimpleSchemas = {
+//   AuditEventSchema, 
+//   CarePlanSchema, 
+//   DeviceSchema, 
+//   EndpointSchema, 
+//   LocationSchema, 
+//   MedicationSchema, 
+//   ObservationSchema, 
+//   OrganizationSchema, 
+//   PatientSchema, 
+//   PractitionerSchema, 
+//   QuestionnaireSchema, 
+//   QuestionnaireResponseSchema
+// }
 
 const _AuditEvent = require('./fhir.schema.json/AuditEvent.schema.json');
 const _CarePlan = require('./fhir.schema.json/CarePlan.schema.json');
@@ -47,7 +47,7 @@ const _Questionnaire = require('./fhir.schema.json/Questionnaire.schema.json');
 const _QuestionnaireResponse = require('./fhir.schema.json/QuestionnaireResponse.schema.json');
 
 
-exports.SimpleSchemas = _SimpleSchemas;
+// exports.SimpleSchemas = _SimpleSchemas;
 exports.AuditEvent = _AuditEvent['default'];
 exports.CarePlan = _CarePlan['default'];
 exports.Device = _Device['default'];
